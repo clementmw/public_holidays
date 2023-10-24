87135f5c3221eafaf17f6b14bf9122187464ac61
clementmwamba <clementmwamba23@gmail.com>
2023-10-24 17:24:54 +0300

    7

--- index.js
+++ index.js
@@ -5,10 +5,17 @@ const apiUrl = "https://holidays.abstractapi.com/v1/?api_key=e9b9a75e66ca4c89a0b
 
 const searchButton = document.querySelector('#searchBtn')
 
-async function checkHoliday (){
+async function checkHoliday(){
     const response = await fetch(apiUrl)
     const data = await response.json();
 
     console.log(data)
+
+
+
+    document.querySelector("#year").innerHTML = data.date_year;
+    document.querySelector("#year").innerHTML = data.date_year;
+    document.querySelector("#year").innerHTML = data.date_year;
+    document.querySelector("#year").innerHTML = data.date_year;
 }
-checkHoliday()
+checkHoliday();
