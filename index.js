5e252b9f371fb57f6899623a61979285f18ea085
clementmwamba <clementmwamba23@gmail.com>
2023-10-24 18:03:15 +0300

    8

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
-const apiKey = "e9b9a75e66ca4c89a0b2730ccdccb768";
-
-
+const apiKey ="e9b9a75e66ca4c89a0b2730ccdccb768" ;
 const apiUrl = "https://holidays.abstractapi.com/v1/?api_key=e9b9a75e66ca4c89a0b2730ccdccb768&country=KE&year=2023&month=10&day=10";
 
-const searchButton = document.querySelector('#searchBtn')
+const searchButton = document.querySelector("#searchBtn")
+const clearButton = document.querySelector("#clearBtn")
 
-async function checkHoliday(){
-    const response = await fetch(apiUrl)
-    const data = await response.json();
+async function addevents (){
 
-    console.log(data)
-
-
-
-    document.querySelector("#year").innerHTML = data.date_year;
-    document.querySelector("#year").innerHTML = data.date_year;
-    document.querySelector("#year").innerHTML = data.date_year;
-    document.querySelector("#year").innerHTML = data.date_year;
+    const response = await fetch (apiUrl)
+   const data = await response.json();
+   
+   console.log(data)
 }
-checkHoliday();
+addevents()
+
+searchButton.addEventListener("click", function() {
+    
+    
+    
+    
+    // alert("i have been clicked")
+})
+
+clearButton.addEventListener("click", function () {
+
+    alert ("destroyer")
+})
