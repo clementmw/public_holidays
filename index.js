6baffdc6cb532369b85a58370716722868456289
clementmwamba <clementmwamba23@gmail.com>
2023-10-24 13:35:16 +0300

    3

---
+++ index.js
@@ -0,0 +1,6 @@
+const apiKey = "e9b9a75e66ca4c89a0b2730ccdccb768";
+
+
+const url = "https://holidays.abstractapi.com/v1/?api_key=e9b9a75e66ca4c89a0b2730ccdccb768&country=KE&year=2023&month=10&day=10";
+
+const searchButton = document.querySelector('#searchBtn')
